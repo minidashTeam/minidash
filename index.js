42bad56b0c25a76bd04490970b26f17ff2cc991b
sik2 <bickwang123@gmail.com>
2024-02-21 13:17:00 +0900

    fix isArr

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-export { checkedArr } from "./src/checkedArr";
+export { isArr } from "./src/isArr";
 export { findOddNums } from "./src/findOddNums";
 export { findEvenNums } from "./src/findEvenNums";
 export { capitalize } from "./src/capitalize"; 
